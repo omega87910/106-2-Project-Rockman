e0258b8c076b96590f64def16ee576e7c306166c
omega87910 <106021054@live.asia.edu.tw>
2018-05-25 09:52:35 +0800

    20180525

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,27 +4,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.lang.Runnable;
 
-class MainFrame extends JFrame{
+class MainFrame extends JFrame implements Runnable{
     private JLabel charcter = new JLabel();
-    private ImageIcon icon =new ImageIcon("Action/default.png");
-    private String icon_name;
-    private Timer[] timer=new Timer[10];
-    private int objx=0,objy=720-185,objw=106,objh=115;
-    private int objbox;
-    private double t=0,speed=0,v=0;
-    private int key;
-    private boolean jumpflag=true,falldowncheck,rightdirection=false,jumpdirection;
-    MainFrame(){
+    private JLabel[] been = new JLabel[3];
+    private ImageIcon icon = new ImageIcon("Action/default-r.png");
+    private Timer[] timer = new Timer[10];
+    private int objx = 0, objy = 720 - 185, objw = 106, objh = 115, v = 90, t = 1;
+    private boolean jumpping = false, direction_Right = true;
+
+    MainFrame() {
         game();
         timer[0].start();
     }
-    private void game(){
-        this.setBounds(300,100,1280,720);
+
+    private void game() {
+        this.setBounds(300, 100, 1280, 720);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(null);
         charcter.setIcon(icon);
-        charcter.setBounds(objx,objy,objw,objh);
+        charcter.setBounds(objx, objy, objw, objh);
         this.add(charcter);
         this.addKeyListener(new KeyListener() {
             @Override
@@ -33,117 +33,139 @@ class MainFrame extends JFrame{
 
             @Override
             public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode()== KeyEvent.VK_RIGHT){
-                    objx += 5;
-                    charcter.setLocation(objx,objy);
-                }
-                if(e.getKeyCode()==KeyEvent.VK_X && jumpflag){
-                    icon =new ImageIcon("Action/jump.png");
-                    objw=icon.getIconWidth();
-                    objh=icon.getIconHeight();
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    if (jumpping) {
+                        icon = new ImageIcon("Action/jump-r.png");
+                    } else {
+                        icon = new ImageIcon("Action/default-r.png");
+                    }
                     charcter.setIcon(icon);
-                    objbox=objy;
-                    jumpflag=false;
+                    objx += 5;
+                    charcter.setLocation(objx, objy);
+                    timer[4].start();
+                    direction_Right = true;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    if (jumpping) {
+                        icon = new ImageIcon("Action/jump-l.png");
+                    } else {
+                        icon = new ImageIcon("Action/default-l.png");
+                    }
+                    charcter.setIcon(icon);
+                    objx -= 5;
+                    charcter.setLocation(objx, objy);
+                    timer[4].start();
+                    direction_Right = false;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_X && !jumpping) {
+                    if (direction_Right) {
+                        icon = new ImageIcon("Action/jump-r.png");
+                    } else {
+                        icon = new ImageIcon("Action/jump-l.png");
+                    }
+                    objw = icon.getIconWidth();
+                    objh = icon.getIconHeight();
+                    charcter.setIcon(icon);
+                    jumpping = true;
                     timer[3].start();
                     timer[0].stop();
                     timer[1].stop();
-                    while (rightdirection){
-                        run();
-                    }
                 }
-                if(e.getKeyCode()==KeyEvent.VK_C ){
-                    if(jumpflag) {
-                        icon = new ImageIcon("Action/shoot.png");
-                        objw = icon.getIconWidth();
-                        objh = icon.getIconHeight();
-                        charcter.setIcon(icon);
+                if (e.getKeyCode() == KeyEvent.VK_C) {
+                    if (jumpping) {
+                        if (direction_Right) {
+                            icon = new ImageIcon("Action/jumpshoot-r.png");
+                        } else {
+                            icon = new ImageIcon("Action/jumpshoot-l.png");
+                        }
+                    } else {
+                        if (direction_Right) {
+                            icon = new ImageIcon("Action/shoot-r.png");
+                        } else {
+                            icon = new ImageIcon("Action/shoot-l.png");
+                        }
                     }
-                    else{
-                        icon = new ImageIcon("Action/jumpshoot.png");
-                        objw = icon.getIconWidth();
-                        objh = icon.getIconHeight();
-                        charcter.setIcon(icon);
-                    }
+                    timer[0].stop();
+                    timer[1].stop();
+                    timer[4].start();
+                    objw = icon.getIconWidth();
+                    objh = icon.getIconHeight();
+                    charcter.setIcon(icon);
                 }
             }
+
             @Override
             public void keyReleased(KeyEvent e) {
-//                if (e.getKeyCode()==KeyEvent.VK_X){
-//                    timer[3].stop();
-//                    timer[4].start();
-//                }
             }
         });
         /////////////////////////closeeye//////////////////////////////
         timer[0] = new Timer(2000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(charcter.getIcon()==icon) {
-                    icon = new ImageIcon("Action/closeeye.png");
+                if (charcter.getIcon() == icon) {
+                    if (direction_Right) {
+                        icon = new ImageIcon("Action/closeeye-r.png");
+                    } else {
+                        icon = new ImageIcon("Action/closeeye-l.png");
+                    }
                     charcter.setIcon(icon);
-                    timer[1].start();
+                    timer[1].restart();
                 }
             }
         });
         ////////////////////////Default//////////////////////////////
-        timer[1] =new Timer(100, new ActionListener() {
+        timer[1] = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                icon =new ImageIcon("Action/default.png");
+                if (direction_Right) {
+                    icon = new ImageIcon("Action/default-r.png");
+                } else {
+                    icon = new ImageIcon("Action/default-l.png");
+                }
                 charcter.setIcon(icon);
                 timer[1].stop();
             }
         });
         ////////////////////////jump///////////////////////
-        timer[3] = new Timer(30, new ActionListener() {
+        timer[3] = new Timer(45, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                v=30;
-                t += 0.1;
-                speed = v-9.8*t;
-                if(speed<=0 && speed>-v){
-                    objy += -speed*t+(1/2)*(-9.8*t*t);
-                }
-                else if(speed>0){
-                    objy -= speed* t +(1 / 2) * (-9.8 * t * t);
-                    if(speed-9.8*(t+0.1)<=0){
-                        speed=0;
+                v = v - 10 * t;
+                if (v <= 0 && v > -90) {
+                    objy -= v * t + (1 / 2) * (-10 * t * t);
+                } else if (v <= -90) {
+                    if (direction_Right) {
+                        icon = new ImageIcon("Action/default-r.png");
+                    } else {
+                        icon = new ImageIcon("Action/default-l.png");
                     }
-                }
-                else if(speed <= -v){
+                    charcter.setIcon(icon);
                     timer[3].stop();
-                    timer[1].start();
-                    timer[0].start();
-                    jumpflag=true;
+                    timer[1].restart();
+                    timer[0].restart();
+                    jumpping = false;
+                    v = 90;
+                } else if (v > 0) {
+                    objy -= v * t + (1 / 2) * (-10 * t * t);
                 }
-
-                charcter.setBounds(objx,objy,objw,objh);
-                charcter.setLocation(objx,objy);
-
+                charcter.setBounds(objx, objy, objw, objh);
+                charcter.setLocation(objx, objy);
             }
         });
-        /////////////////////////jumpfalldown/////////////////////////////
-        timer[4] =new Timer(30, new ActionListener() {
+        /////////////////////////shoot/////////////////////////////
+        timer[4] = new Timer(500, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                charcter.setBounds(objx,objy,objw,objh);
-                charcter.setLocation(objx,objy);
-                if(objy==720-185){
-                    timer[4].stop();
+                if (!jumpping) {
+                    timer[0].restart();
                     timer[1].start();
-                    timer[0].start();
-                    jumpflag=true;
-                    t=0;
                 }
             }
         });
-        ////////////////////////walk/////////////////////////////////////////
-
     }
-    private void run(){
-        if(key==KeyEvent.VK_RIGHT){
-            objx += 5;
-        }
+
+    @Override
+    public void run() {
+
     }
 }
