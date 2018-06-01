c361654ad6110605e9342bc4f4771c06f2751af9
omega87910 <106021054@live.asia.edu.tw>
2018-06-02 02:26:22 +0800

    20180602

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,74 +1,55 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.lang.Runnable;
-class MainFrame extends JFrame{
-    Rockman rockman = new Rockman() ;
-    private JLabel charcter = new JLabel();
+class MainFrame extends JFrame {
+    Rockman rockman = new Rockman();
     private JLabel background = new JLabel();
-    private JLabel[] been = new JLabel[3];
     private ImageIcon icon = new ImageIcon("Action/default-r.png");
-    private Timer[] timer = new Timer[10];
-    private int objx = 0, objy = 720 - 205 , objw = 106, objh = 115, v = 90, t = 1;
-    private boolean jumpping = false, direction_Right = true , shooting=false,running=false,threadstart=false;
-    private int key ;
-    Thread thread =new Thread(rockman);
-
+    Thread thread = new Thread(rockman);
     MainFrame() {
         game();
         thread.start();
-//        timer[0].start();
     }
     private void game() {
-        this.setBounds(300, 100, 1280-10, 720);
+        this.setBounds(300, 100, 1280 - 10, 720);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(null);
+        this.setResizable(false);
         rockman.setIcon(icon);
-        rockman.setBounds(objx, objy+12, objw, objh);
-        rockman.setObjx(objx);
-        rockman.setObjy(objy);
-        this.add(charcter);
+        rockman.setBounds(this.getX(), this.getY() + 12, this.getWidth(), this.getHeight());
         this.add(rockman);
         this.add(background);
         background.setIcon(new ImageIcon("./background.png"));
-        background.setBounds(0,-20,getWidth(),getHeight());
+        background.setBounds(0, -20, getWidth(), getHeight());
         //////////////////////////key/////////////////////////////////////
         this.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
             }
+
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                        if (rockman.getJumpping()) {
-                            rockman.setIcon(new ImageIcon("Action/jump-r.png"));
-                        } else {
-                            rockman.setIcon(new ImageIcon("Action/default-r.png"));
-                        }
-                        rockman.run_start();
-                        rockman.setDirection_Right(true);
+                    if (rockman.getJumpping()) {
+                        rockman.setIcon(new ImageIcon("Action/jump-r.png"));
+                    }
+                    rockman.run_start();
+                    rockman.setDirection_Right(true);
                 }
                 if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                        if (rockman.getJumpping()) {
-                            rockman.setIcon(new ImageIcon("Action/jump-l.png"));
-                        } else {
-                            rockman.setIcon(new ImageIcon("Action/default-l.png"));
-                        }
-                        rockman.run_start();
-                        rockman.setDirection_Right(false);
+                    if (rockman.getJumpping()) {
+                        rockman.setIcon(new ImageIcon("Action/jump-l.png"));
+                    }
+                    rockman.run_start();
+                    rockman.setDirection_Right(false);
                 }
                 if (e.getKeyCode() == KeyEvent.VK_X && !rockman.getJumpping()) {
                     rockman.setJumpping(true);
-                        if(rockman.getDirection_Right()){
-                            rockman.setIcon(new ImageIcon("Action/jump-r.png"));
-                        }else {
-                            rockman.setIcon(new ImageIcon("Action/jump-l.png"));
-                        }
-//                    timer[0].stop();
-//                    timer[1].stop();
+                    if (rockman.getDirection_Right()) {
+                        rockman.setIcon(new ImageIcon("Action/jump-r.png"));
+                    } else {
+                        rockman.setIcon(new ImageIcon("Action/jump-l.png"));
+                    }
                 }
 //                if (e.getKeyCode() == KeyEvent.VK_C) {
 //                    if (jumpping) {
@@ -94,12 +75,12 @@ class MainFrame extends JFrame{
 //                    rockman.setIcon(icon);
 //                }
             }
+
             @Override
             public void keyReleased(KeyEvent e) {
-                if(e.getKeyCode()==KeyEvent.VK_X) {
+                if (e.getKeyCode() == KeyEvent.VK_X) {
                 }
-                if(e.getKeyCode()==KeyEvent.VK_RIGHT || e.getKeyCode()==KeyEvent.VK_LEFT){
-                    running=false;
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_LEFT) {
                     rockman.run_stop();
                 }
             }
@@ -135,43 +116,27 @@ class MainFrame extends JFrame{
 //
 //        }
 //        });
-        ////////////////////////jump///////////////////////
-//        timer[3] = new Timer(0, new ActionListener() {
+        /////////////////////////shoot/////////////////////////////
+//        timer[4] = new Timer(400, new ActionListener() {
 //            @Override
 //            public void actionPerformed(ActionEvent e) {
-//
-//                if(!rockman.getJumpping()) {
-//
-//                    timer[3].stop();
-//                    timer[0].start();
-//                    jumpping = false;
+//                if (jumpping) {
+//                    if (direction_Right) {
+//                        rockman.setIcon(new ImageIcon("Action/jump-r.png"));
+//                    } else {
+//                        rockman.setIcon(new ImageIcon("Action/jump-l.png"));
+//                    }
+//                } else {
+//                    if (direction_Right) {
+//                        rockman.setIcon(new ImageIcon("Action/default-r.png"));
+//                    } else {
+//                        rockman.setIcon(new ImageIcon("Action/default-l.png"));
+//                    }
 //                }
+//                rockman.setLocation(objx, objy);
+////                timer[0].start();
+////                timer[4].stop();
 //            }
 //        });
-        /////////////////////////shoot/////////////////////////////
-        timer[4] = new Timer(400, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (jumpping) {
-                    if(direction_Right){
-                        rockman.setIcon(new ImageIcon("Action/jump-r.png"));
-                    }
-                    else{
-                        rockman.setIcon(new ImageIcon("Action/jump-l.png"));
-                    }
-                }
-                else {
-                    if(direction_Right) {
-                        rockman.setIcon(new ImageIcon("Action/default-r.png"));
-                    }
-                    else {
-                        rockman.setIcon(new ImageIcon("Action/default-l.png"));
-                    }
-                }
-                rockman.setLocation(objx,objy);
-//                timer[0].start();
-//                timer[4].stop();
-            }
-        });
     }
 }
--- src/Rockman.java
+++ src/Rockman.java
@@ -1,20 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 public class Rockman extends JLabel implements Runnable {
-    protected int objx, objy, v = 65, t = 1;
+    protected int objx = 0, objy = 720 - 205, v = 65, t = 1, runstep = -6;
     protected boolean running, jumpping, direction_Right;
+    private Timer[] timer = new Timer[2];
 
     public Rockman() {
     }
 
     public void setObjx(int objx1) {
         this.objx = objx1;
-    }
-
-    public int getv() {
-        return v;
     }
 
     public void setDirection_Right(boolean direction_Right1) {
@@ -50,22 +49,84 @@ public class Rockman extends JLabel implements Runnable {
         while (true) {
             if (running) {
                 if (direction_Right) {
-                    objx += 10;
-                    System.out.println("666");
+                    System.out.println("right");
+                    if (runstep >= 0) {
+                        objx += 10;
+                        if (!jumpping) {
+                            switch (runstep % 40) {
+                                case 0:
+                                    Rockman.this.setIcon(new ImageIcon("Action/walk" + 1 + "-r.png"));
+                                    Rockman.this.setBounds(objx, objy + 21, this.getIcon().getIconWidth(), this.getIcon().getIconHeight());
+                                    break;
+                                case 10:
+                                    Rockman.this.setIcon(new ImageIcon("Action/walk" + 2 + "-r.png"));
+                                    break;
+                                case 20:
+                                    Rockman.this.setIcon(new ImageIcon("Action/walk" + 3 + "-r.png"));
+                                    Rockman.this.setBounds(objx, objy + 21, this.getIcon().getIconWidth(), this.getIcon().getIconHeight());
+                                    break;
+                                case 30:
+                                    Rockman.this.setIcon(new ImageIcon("Action/walk" + 2 + "-r.png"));
+                                    break;
+                            }
+                        }
+                    } else {
+                        objx += 1;
+                        if (!jumpping) {
+                            Rockman.this.setIcon(new ImageIcon("Action/walk" + 0 + "-r.png"));
+                        }
+                    }
                 } else {
-                    objx -= 10;
-                    System.out.println("666");
+                    System.out.println("left");
+                    if (runstep >= 0) {
+                        objx -= 10;
+                        if (!jumpping) {
+                            switch (runstep % 40) {
+                                case 0:
+                                    Rockman.this.setIcon(new ImageIcon("Action/walk" + 1 + "-l.png"));
+                                    Rockman.this.setBounds(objx, objy + 21, this.getIcon().getIconWidth(), this.getIcon().getIconHeight());
+                                    break;
+                                case 10:
+                                    Rockman.this.setIcon(new ImageIcon("Action/walk" + 2 + "-l.png"));
+                                    break;
+                                case 20:
+                                    Rockman.this.setIcon(new ImageIcon("Action/walk" + 3 + "-l.png"));
+                                    Rockman.this.setBounds(objx, objy + 21, this.getIcon().getIconWidth(), this.getIcon().getIconHeight());
+                                    break;
+                                case 30:
+                                    Rockman.this.setIcon(new ImageIcon("Action/walk" + 2 + "-l.png"));
+                                    break;
+                            }
+                        }
+                    } else {
+                        objx -= 1;
+                        if (!jumpping) {
+                            Rockman.this.setIcon(new ImageIcon("Action/walk" + 0 + "-l.png"));
+                        }
+                    }
                 }
-                Rockman.this.setBounds(objx, objy+12, this.getIcon().getIconWidth(), this.getIcon().getIconHeight());
+                if ((runstep % 40 >= 0 && runstep % 40 < 10) || (runstep % 40 >= 20 && runstep % 40 < 30)) {
+                    Rockman.this.setBounds(objx, objy + 21, this.getIcon().getIconWidth(), this.getIcon().getIconHeight());
+                } else {
+                    Rockman.this.setBounds(objx, objy + 12, this.getIcon().getIconWidth(), this.getIcon().getIconHeight());
+                }
+                runstep += 2;
             } else {
+                runstep=0;
+                if (!jumpping) {
+                    if (direction_Right) {
+                        Rockman.this.setIcon(new ImageIcon("Action/default-r.png"));
+                    } else {
+                        Rockman.this.setIcon(new ImageIcon("Action/default-l.png"));
+                    }
+                    Rockman.this.setBounds(objx, objy + 12, this.getIcon().getIconWidth(), this.getIcon().getIconHeight());
+                    runstep = -6;
+                }
                 System.out.println("stop");
             }
-            try {
-                Thread.sleep(30);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             if (jumpping) {
+                runstep=0;
+                System.out.println("jump");
                 v = v - 5 * t;
                 if (v <= 0 && v > -65) {
                     objy -= v * t + (1 / 2) * (-10 * t * t);
@@ -80,9 +141,13 @@ public class Rockman extends JLabel implements Runnable {
                 } else if (v > 0) {
                     objy -= v * t + (1 / 2) * (-10 * t * t);
                 }
-                Rockman.this.setBounds(objx, objy+12, Rockman.this.getIcon().getIconWidth(), Rockman.this.getIcon().getIconHeight());
+                Rockman.this.setBounds(objx, objy + 12, Rockman.this.getIcon().getIconWidth(), Rockman.this.getIcon().getIconHeight());
+            }
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
-
 }
